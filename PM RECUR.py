97bee0e65396351cc30d5f193e4cde29d1d5fb4c
ahmedsadid <42942592+ahmedsadid@users.noreply.github.com>
2020-01-25 19:25:04 +0600

    Update PM RECUR.py

--- PM RECUR.py
+++ PM RECUR.py
@@ -1,3 +1,11 @@
+# TASK 3.5
+"""---  write a recursive algorithm that would output each letter in a string, from the
+beginning, one at a time. The string is passed as a parameter --- """
+# AUTHOR : ZUBAYER AHMED SADID
+
+# inStr : STRING
+# L     : INTEGER
+
 def outChars(inStr):
     L = len(inStr)
     
@@ -7,6 +15,5 @@ def outChars(inStr):
         print(inStr[0])
         inStr = inStr[-(L-1):]
         outChars(inStr)
-
 
 outChars("example")
